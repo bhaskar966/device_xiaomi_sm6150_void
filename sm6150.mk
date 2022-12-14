c30d4a75c8c3e5b07800fe2da43267a783beb40a
bhaskar966 <deybhaskar999@gmail.com>
2022-12-14 19:46:55 +0530

    sm6150: Awaken OS Bringup

--- sm6150.mk
+++ sm6150.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-bliss
+    $(LOCAL_PATH)/overlay-awaken
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
