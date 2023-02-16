f33d55b3f61899657b52d1d9d283eff47d0c45d0
bhaskar966 <deybhaskar999@gmail.com>
2023-02-16 23:11:19 +0530

    sm6150-common : VoidUI Bringup
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-cherish
+    $(LOCAL_PATH)/overlay-void
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
